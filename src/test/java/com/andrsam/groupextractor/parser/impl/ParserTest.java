30fd95a1ef36385a4261e21056656e506ee168a4
aIsamoylov <AISamoylov@IBS.RU>
2019-09-24 17:43:50 +0300

    start developing test

--- src/test/java/com/andrsam/groupextractor/parser/impl/ParserTest.java
+++ src/test/java/com/andrsam/groupextractor/parser/impl/ParserTest.java
@@ -7,15 +7,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class ParserTest {
+    public static final String TEST_STRING = "\"83822459695\";\"\";\"200000087201031\"";
     private IParser parser = new Parser();
     private final Map<String, List<String>> map = new HashMap<>();
 
     @Test
     public void parse() {
-        Map<String, List<String>> result = parser.parse("\"83822459695\";\"\";\"200000087201031\"", map);
+        Map<String, List<String>> result = parser.parse(TEST_STRING, map);
         assertNull(result.get(""));
+        assertEquals(TEST_STRING, result.get("83822459695").get(0));
+        assertEquals(TEST_STRING, result.get("200000087201031").get(0));
     }
 }
---
+++ src/test/java/com/andrsam/groupextractor/reader/impl/FileProcessorTest.java
@@ -0,0 +1,11 @@
+package com.andrsam.groupextractor.reader.impl;
+
+import org.junit.Test;
+
+public class FileProcessorTest {
+
+    @Test
+    public void read() {
+
+    }
+}
