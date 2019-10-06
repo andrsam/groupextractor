4439acff3c74a88654a57daf798de242ba7c9465
Andrey Samoylov <andrsbox@gmail.com>
2019-10-06 22:29:11 +0300

    replace constants to array

--- src/test/java/com/andrsam/groupextractor/reader/impl/FileProcessorTest.java
+++ src/test/java/com/andrsam/groupextractor/reader/impl/FileProcessorTest.java
@@ -4,40 +4,48 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
 public class FileProcessorTest {
-    public static final String STR_DUPLICATES = "\"83178111069\";\"100000094623698\";\"200000064948582\"\n" +
-            "\"83822459695\";\"\";\"200000087201031\"\n" +
-            "\"83178111069\";\"100000815068665\";\"200000605588834\"\n" +
-            "\"\";\"100000661125097\";\"200000171650768\"\n" +
-            "\"83273885690\";\"100000372967409\";\"200000269854191\"\n" +
-            "\"83408667148\";\"100000815068665\";\"200000827683220\"\n" +
-            "\"83178111069\";\"100000094623697\";\"200000064948582";
 
-    public static final String STR_NO_DUPLICATES = "\"83171375084\";\"100000003421956\";\"\"\n" +
-            "\"83620410120\";\"100000227778156\";\"\"\n" +
-            "\"83918105990\";\"100000043186771\";\"200000590456885\"\n" +
-            "\"83120897035\";\"100000785378490\";\"200000999920329\"\n" +
-            "\"83710725507\";\"100000901641382\";\"200000461415093\"\n" +
-            "\"83552471951\";\"100000395408945\";\"200000858070199\"\n" +
-            "\"83384681155\";\"100000854416515\";\"200000670938412\"";
+    private static final String[] STR_DUPLICATES = new String[]{
+            "\"83178111069\";\"100000094623698\";\"200000064948582\"",
+            "\"83822459695\";\"\";\"200000087201031\"",
+            "\"83178111069\";\"100000815068665\";\"200000605588834\"",
+            "\"\";\"100000661125097\";\"200000171650768\"",
+            "\"83273885690\";\"100000372967409\";\"200000269854191\"",
+            "\"83408667148\";\"100000815068665\";\"200000827683220\"",
+            "\"83178111069\";\"100000094623697\";\"200000064948582" };
+
+    private static final String[] STR_NO_DUPLICATES = new String[]{"\"83171375084\";\"100000003421956\";",
+            "\"83620410120\";\"100000227778156\";\"\"",
+            "\"83918105990\";\"100000043186771\";\"200000590456885\"",
+            "\"83120897035\";\"100000785378490\";\"200000999920329\"",
+            "\"83710725507\";\"100000901641382\";\"200000461415093\"",
+            "\"83552471951\";\"100000395408945\";\"200000858070199\"",
+            "\"83384681155\";\"100000854416515\";\"200000670938412\"" };
 
     private FileProcessor fileProcessor = new FileProcessor();
 
     @Test
     public void readDuplicates() throws IOException {
-        Map<String, List<String>> map = fileProcessor.read(new StringReader(STR_DUPLICATES));
+        Map<String, List<String>> map = fileProcessor.read(new StringReader(arrayToString(STR_DUPLICATES)));
         assertFalse(map.isEmpty());
         assertEquals(3, map.get("\"83178111069\"").size());
     }
 
     @Test
     public void readNoDuplicates() throws IOException {
-        Map<String, List<String>> map = fileProcessor.read(new StringReader(STR_NO_DUPLICATES));
+        Map<String, List<String>> map = fileProcessor.read(new StringReader(arrayToString(STR_NO_DUPLICATES)));
         assertTrue(map.isEmpty());
+    }
+
+    private String arrayToString(String[] arr) {
+        return Arrays.stream(arr).collect(Collectors.joining(System.lineSeparator()));
     }
 }
