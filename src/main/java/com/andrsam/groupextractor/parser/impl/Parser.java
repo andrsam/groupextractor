3e22d7b5553248688c42d9aa1be0b9fbedbc487c
aIsamoylov <AISamoylov@IBS.RU>
2019-10-22 15:23:00 +0300

    change data type of map value to Set - 2

--- src/main/java/com/andrsam/groupextractor/parser/impl/Parser.java
+++ src/main/java/com/andrsam/groupextractor/parser/impl/Parser.java
@@ -3,13 +3,12 @@ package com.andrsam.groupextractor.parser.impl;
 import com.andrsam.groupextractor.parser.IParser;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Parser implements IParser {
     @Override
-    public Map<String, Set<String>> parse(String source, Map<String, List<String>> sourceMap) {
+    public Map<String, Set<String>> parse(String source, Map<String, Set<String>> sourceMap) {
         String[] values = source.split(";");
         for (String value : values) {
             if (!value.equals("\"\"")) {
